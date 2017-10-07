daf9030dff57a0063b6c96b135bcbba4fca3b578
Akmulla <d4n73@yandex.ru>
2017-10-07 18:31:53 +0600

    basics

--- Ship/Assets/Scripts/GameController.cs
+++ Ship/Assets/Scripts/GameController.cs
@@ -2,15 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour {
+public enum GameState { Game,Pause};
 
-	// Use this for initialization
-	void Start () {
+public class GameController : MonoBehaviour
+{
+    public static GameController gc;
+    float saved_time_scale = 1.0f;
+    public GameState State { get; set; }
+	
+    void Awake()
+    {
+        gc = GetComponent<GameController>();
+        State = GameState.Game;
+    }
+
+	void Start ()
+    {
 		
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+
+    public void Pause()
+    {
+        saved_time_scale = Time.timeScale;
+        Time.timeScale = 0.0f;
+        State = GameState.Pause;
+        //UIController.ui.UpdateUI();
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = saved_time_scale;
+        State = GameState.Game;
+        //UIController.ui.UpdateUI();
+    }
 }
---
+++ Ship/Assets/Scripts/Ship.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ShipState { Idle,Prepare,Sail};
+
+public class Ship : MonoBehaviour
+{
+    public ShipState State { get; set; }
+    Rigidbody2D rb;
+    Vector3 start_point;
+
+    void Awake()
+    {
+        //State = InputState.Default;
+        State = ShipState.Idle;
+    }
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (GameController.gc.State != GameState.Game)
+            return;
+
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        mousePosition.z = 0.0f;
+
+        if ((Input.GetMouseButtonDown(0))&&(State==ShipState.Idle))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            print(hit.collider);
+            if (hit.collider!=null)
+            {
+                if (hit.collider.gameObject.CompareTag("Ship"))
+                {
+                    State = ShipState.Prepare;
+                }
+
+            }
+        }
+
+        if ( (Input.GetMouseButton(0)) && (State == ShipState.Prepare) )
+        {
+            transform.position = mousePosition;
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            State = ShipState.Idle;
+        }
+    }
+
+    public void Throw()
+    {
+
+    }
+
+    public void RotateBackToPoint(Vector3 point)
+    {
+        point.z = 0;
+        float angle = Vector2.Angle(Vector2.right, point - transform.position);
+        //transform.eulerAngles = new Vector3(0f, 0f, transform.position.y < point.y ? angle : -angle);
+        transform.eulerAngles = new Vector3(0f, 0f, 
+            transform.position.y < point.y ? angle + 180.0f : -angle + 180.0f);
+    }
+}
