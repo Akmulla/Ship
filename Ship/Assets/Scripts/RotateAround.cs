a6198b80f632be621e8cac604e193cea971b06a9
Akmulla <d4n73@yandex.ru>
2017-10-10 04:43:01 +0600

    fidget spinner

---
+++ Ship/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour
+{
+    public float grad;
+    Transform tran;
+	
+    void Start()
+    {
+        tran = GetComponent<Transform>();
+    }
+
+	void Update ()
+    {
+        tran.Rotate(new Vector3(0.0f, 0.0f, grad * Time.deltaTime));
+	}
+}
