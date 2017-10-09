16f0999c068638286845735cdc34ccb948d71ced
Akmulla <d4n73@yandex.ru>
2017-10-10 05:26:10 +0600

    game over

---
+++ Ship/Assets/Scripts/StartScene.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScene : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (GameController.gc != null)
+            Destroy(GameController.gc.gameObject);
+
+        if (UIManager.ui != null)
+            Destroy(UIManager.ui.gameObject);
+    }
+	
+	public void BeginGame()
+    {
+        SceneManager.LoadScene("Lvl_0");
+    }
+}
